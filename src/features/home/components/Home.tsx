a1d800f0f12b66f20ff51259034f0bb5f736057f
ankur-varma-capgemini <ankur.varma@capgemini.com>
2026-08-27 10:47:29 -0400

    setting up Tanstack Router

---
+++ src/features/home/components/Home.tsx
@@ -0,0 +1,5 @@
+const Home = () => {
+  return <div>Welcome to AI Hub</div>
+}
+
+export default Home
---
+++ src/routes/__root.tsx
@@ -0,0 +1,8 @@
+import { createRootRoute } from '@tanstack/react-router'
+
+import '../styles.css'
+import Root from '#/features/root/components/Root'
+
+export const Route = createRootRoute({
+  component: Root,
+})
