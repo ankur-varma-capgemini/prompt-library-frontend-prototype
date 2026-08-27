af58c11907af1461f9d936380349aa26bed2ea14
ankur-varma-capgemini <ankur.varma@capgemini.com>
2026-08-27 11:08:45 -0400

    changing the formatting rules

--- src/main.tsx
+++ src/main.tsx
@@ -1,22 +1,22 @@
-import { RouterProvider, createRouter } from '@tanstack/react-router'
-import { routeTree } from './routeTree.gen'
-import { createRoot } from 'react-dom/client'
-import { StrictMode } from 'react'
+import { RouterProvider, createRouter } from "@tanstack/react-router";
+import { routeTree } from "./routeTree.gen";
+import { createRoot } from "react-dom/client";
+import { StrictMode } from "react";
 
 const router = createRouter({
   routeTree,
-  defaultPreload: 'intent',
+  defaultPreload: "intent",
   scrollRestoration: true,
-})
+});
 
-declare module '@tanstack/react-router' {
+declare module "@tanstack/react-router" {
   interface Register {
-    router: typeof router
+    router: typeof router;
   }
 }
 
-createRoot(document.getElementById('app')!).render(
+createRoot(document.getElementById("app")!).render(
   <StrictMode>
     <RouterProvider router={router} />
   </StrictMode>,
-)
+);
