9788fb3c1c5f44f356e33b7c666d95fa612faa2d
ankur-varma-capgemini <ankur.varma@capgemini.com>
2026-08-28 12:09:51 -0400

    positioning the header

--- src/features/header/components/Header.tsx
+++ src/features/header/components/Header.tsx
@@ -4,7 +4,7 @@ import Panel from "./Panel";
 
 const Header = (): JSX.Element => {
   return (
-    <header className="fixed inset-x-0 top-0 z-[60] flex h-16 items-center justify-between gap-4 border-b-[3px] border-[#DA2127] bg-white px-5">
+    <header className="flex h-16 items-center justify-between gap-4 border-b-[3px] border-[#DA2127] bg-white px-5">
       <Logo />
       <Panel />
     </header>
